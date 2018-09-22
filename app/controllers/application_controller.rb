2e47982aae2ed171326f028b64f5fcb86b3692e4
Zheng <yzheng@aus1m-yzheng.rws.ad.ea.com>
2018-09-22 16:54:15 -0500

    replace root with groups#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
-#class ApplicationController < ActionController::Base
-  #protect_from_forgery with: :exception
-#end
-class WelcomeController < ApplicationController
-  def index
-    flash[:notice] = "早安！你好！"
-  end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 end
